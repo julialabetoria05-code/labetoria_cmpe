49af827beb023a4b80cbdd4b77f5cd854eff09c3
julialabetoria05-code <julialabetoria05@gmail.com>
2026-01-20 22:34:27 +0800

    add new test file

---
+++ WEEK 2/firstcodeee.js
@@ -0,0 +1,12 @@
+console.log("BS ECE 1-4 demo");
+
+function countdown(num) {
+    console.log(num);
+    if (num > 0) {
+        setTimeout(function () {
+            countdown(num - 1);
+        }, 1000);
+    }
+}
+
+countdown(10);
